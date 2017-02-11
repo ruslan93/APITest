ca683d92e859362fa87fb6d986f866a004aa513d
Ruslan <palapa.ruslan@gmail.com>
2017-02-11 16:19:53 +0200

    initital mysql

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -1,31 +1,57 @@
-import URI
-import HTTP
 import Vapor
 import VaporMySQL
 
+
 let drop = Droplet(
-    availableMiddleware: ["cors" : CorsMiddleware()],
-    serverMiddleware: ["file", "cors"],
-    preparations: [Todo.self],
+    preparations: [Acronym.self, Employee.self],
     providers: [VaporMySQL.Provider.self]
 )
 
-// MARK: Landing Pages
-
-drop.get { _ in try drop.view.make("welcome") }
-
-// MARK: Tests Redirect
-
-drop.get("tests") { request in
-    guard let baseUrl = request.baseUrl else { throw Abort.badRequest }
-    let todosUrl = baseUrl + "todos"
-    return Response(redirect: "http://todobackend.com/specs/index.html?\(todosUrl)")
+// get options
+drop.get ("options") { request in
+    return try JSON(node: [
+        "message" : "options",
+        "owner" : "ruslan"
+        ])
 }
 
-// MARK: /todos/
 
-drop.grouped(TodoURLMiddleware()).resource("todos", TodoController())
+// add new employee
+drop.post ("employee", "new") { request in
+    guard let name = request.data["name"]?.string else {
+        return try JSON(node: [
+            "error":"name"
+            ])
+    }
+    guard let age = request.data["age"]?.int else {
+        return try JSON(node: [
+            "error":"age"
+            ])
+    }
+    var employee = Employee(name: name, age: age)
+    try employee.save()
+    return try employee.makeResponse()
+}
 
-// MARK: Serve
+// get all employees
+drop.get ("employee", "all") { request in
+    guard let name = request.data["name"]?.string else {
+        return try JSON(node: Employee.query().all().makeNode())
+    }
+    return try JSON(node: Employee.query().filter("name", name).all().makeNode())
+}
+
+// update
+drop.post("employee", "update") { request in
+    guard let ID = request.data["id"]?.int, let newName = request.data["name"]?.string, var first = try Employee.query().filter("id", ID).first() else {
+        return try JSON(node: [
+            "error":"user with ID is missing "
+            ])
+    }
+    first.name = newName
+    try first.save()
+    return try first.makeResponse()
+}
+
 
 drop.run()
